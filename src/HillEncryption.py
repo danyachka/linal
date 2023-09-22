16f572545460fe64e5744a69d759717bd8ce148a
DanilaKisikov <kisikov03@mail.ru>
2023-09-23 00:31:54 +0300

    Do first part

--- src/HillEncryption.py
+++ src/HillEncryption.py
@@ -1,35 +1,55 @@
-import numpy as np
+import random
+import sympy as sp
 import math
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ",
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 
-def crypt(key: np.matrix, data: str) -> str:
+def encrypt(key: sp.Matrix, data: str) -> str:
     numbers: [int] = getNumbers(data)
     resultIndexes: [int] = []
 
-    length = len(key)
+    length = len(key.row(0))
     count = math.ceil(len(data) / length)
 
     for i in range(count):
         vector = []
         for j in range(length):
-            vector.append(numbers[length * i + j])
+            try:
+                vector.append(numbers[length * i + j])
+            except:
+                vector.append(26)
 
-        vector = np.matrix([vector])
+        vector = sp.Matrix([vector]).T
+        finalVector: sp.Matrix = key * vector
 
-        vector = np.dot(vector, key)
-
-        resultIndexes += list(vector.flat)
+        for j in range(length):
+            resultIndexes.append(finalVector[j, 0])
 
     alphabetLen = len(alphabet)
-    for i in range(len(resultIndexes)):
+    for i in range(0, len(resultIndexes)):
         resultIndexes[i] = resultIndexes[i] % alphabetLen
 
     result = getLetters(resultIndexes)
 
     return result
+
+
+def decrypt(key: sp.Matrix, data: str) -> str:
+    mod = len(alphabet)
+
+    det = key.det()
+    key = key.adjugate() % mod
+
+    det = det % mod
+
+    invDet = pow(det, -1, mod)
+
+    key = key * invDet
+
+    return encrypt(key, data)
 
 
 def getNumbers(data) -> [int]:
@@ -47,6 +67,24 @@ def getLetters(data) -> str:
     result = ""
 
     for number in data:
-        result += alphabet[number]
+        result += alphabet[int(number)]
 
     return result
+
+
+def changeRandomLetters(text: str) -> str:
+    length = len(text)
+
+    count = 3
+    if length < 4:
+        count = 1
+
+    size = len(alphabet)
+    for j in range(count):
+        i = random.randint(0, length - 1)
+        new = alphabet[random.randint(0, size - 1)]
+        text = text[:i] + new + text[i + 1:]
+
+    return text
+
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
-import numpy as np
+import sympy as sp
 import src.HillEncryption as HillEncryption
 
+
+def firstPart():
+    print("Enter text to encrypt")
+    data = input()
+
+    keys = [sp.Matrix([[1, 3], [2, 1]]), sp.Matrix([[1, 3, 5], [11, 4, 9], [2, 21, 18]]),
+            sp.Matrix([[18, 3, 5, 16], [11, 4, 9, 27], [2, 21, 18, 13], [4, 14, 16, 23]])]
+
+    for i in range(len(keys)):
+        text = data
+        key = keys[i]
+        print("\n\nMatrix with side " + str(len(key)))
+        print("Determinant is " + str(key.det()))
+        text = HillEncryption.encrypt(key.copy(), text)
+        print("Encrypted text: " + text)
+        newText = HillEncryption.changeRandomLetters(text)
+        print("Changed encrypted text: " + newText)
+        print("Decryption of changed text: " + HillEncryption.decrypt(key.copy(), newText))
+        print("Decryption of real text: " + HillEncryption.decrypt(key.copy(), text))
+
+
 if __name__ == '__main__':
-
-    key: np.matrix = np.matrix([[1, 3], [2, 1]])
-
-    string = "huy huy huy "
-
-    print(HillEncryption.crypt(key, string))
+    print(len(HillEncryption.alphabet))
+    firstPart()
